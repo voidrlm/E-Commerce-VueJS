98a9b15ea192e8236ea233ddbcfd4bbb19519876
voidrlm <varjun609@gmail.com>
2022-12-20 02:01:51 +0530

    Removed navigation drawer

--- src/components/navigation/navBar.vue
+++ src/components/navigation/navBar.vue
@@ -1,33 +1,5 @@
 <template>
-  <nav>
-    <v-navigation-drawer
-      v-model="showNavbarDrawer"
-      app
-      class="accent"
-      width="245"
-    >
-      <v-list nav rounded class="mt-2 pa-5">
-        <v-list-item
-          v-for="item in items"
-          :key="item.title"
-          router
-          :to="item.route"
-          class="mb-3"
-        >
-          <v-list-item-icon class="mx-5">
-            <v-icon size="30">{{ item.icon }}</v-icon>
-          </v-list-item-icon>
-
-          <v-list-item-content>
-            <v-list-item-title class="body-2 font-weight-bold">{{
-              item.title
-            }}</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <app-bar @openCloseNavBar="showNavbarDrawer = !showNavbarDrawer" />
-  </nav>
+  <app-bar />
 </template>
 
 <script>
@@ -37,14 +9,6 @@ export default {
     appBar,
   },
 
-  data: () => ({
-    showNavbarDrawer: true,
-    items: [
-      { title: "Dashboard", icon: "mdi-home", route: "/dashboard" },
-      { title: "Product", icon: "mdi-alert", route: "/product" },
-      { title: "Cart", icon: "mdi-cart", route: "/cart" },
-      { title: "Search", icon: "mdi-calendar-blank", route: "/search" },
-    ],
-  }),
+  data: () => ({}),
 };
 </script>
