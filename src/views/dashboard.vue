8a738dc257f1a207f05489cd519f951bfdbcc3c1
voidrlm <varjun609@gmail.com>
2023-01-13 12:40:56 +0530

    Changes in dashboard alignment

--- src/views/dashboard.vue
+++ src/views/dashboard.vue
@@ -59,7 +59,7 @@
             </v-row>
           </v-carousel-item>
         </v-carousel>
-        <div class="pl-4 pr-4 row">
+        <div class="mt-2 row">
           <div class="col-md-6 col-sm-6 col-xs-12">
             <v-card>
               <v-img
@@ -95,7 +95,7 @@
             </v-card>
           </div>
         </div>
-        <div class="pl-4 pr-4 row">
+        <div class="row">
           <div class="col-md-4 col-sm-4 col-xs-12">
             <v-card outlined>
               <v-img
@@ -157,134 +157,110 @@
             </v-card>
           </div>
         </div>
-        <v-container>
-          <v-row no-gutters>
-            <v-col :cols="12">
-              <v-card-text class="" tile outlined>
-                <v-card-title class="subheading">Deals of the Day</v-card-title>
-                <v-divider></v-divider>
-                <div class="row">
-                  <div class="col-12 col-md-3 col-sm-6 col-xs-6 text-center">
-                    <v-hover v-slot:default="{ hover }" open-delay="200">
-                      <v-card :elevation="hover ? 16 : 2">
-                        <v-img
-                          class="white--text align-end"
-                          height="200px"
-                          :src="'https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg'"
-                        >
-                          <v-card-title>Bags & Purses </v-card-title>
-                        </v-img>
 
-                        <v-card-text class="text--primary text-center">
-                          <div>Upto 60% + Extra 10%</div>
-                          <div>Baggit, Zara, Fossil</div>
-                        </v-card-text>
+        <v-card-title class="subheading justify-center"
+          >Deals of the Day</v-card-title
+        >
+        <v-divider></v-divider>
+        <div class="row mb-3">
+          <div class="col-12 col-md-3 col-sm-6 col-xs-6 text-center">
+            <v-hover v-slot:default="{ hover }" open-delay="200">
+              <v-card :elevation="hover ? 16 : 2">
+                <v-img
+                  class="white--text align-end"
+                  height="200px"
+                  :src="'https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg'"
+                >
+                  <v-card-title>Bags & Purses </v-card-title>
+                </v-img>
 
-                        <div class="text-center">
-                          <v-btn
-                            to="/search"
-                            class="ma-2"
-                            outlined
-                            color="info"
-                          >
-                            Explore
-                          </v-btn>
-                        </div>
-                      </v-card>
-                    </v-hover>
-                  </div>
-                  <div class="col-12 col-md-3 col-sm-6 col-xs-6 text-center">
-                    <v-hover v-slot:default="{ hover }" open-delay="200">
-                      <v-card :elevation="hover ? 16 : 2">
-                        <v-img
-                          class="white--text align-end"
-                          height="200px"
-                          :src="'https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg'"
-                        >
-                          <v-card-title>T-Shirt </v-card-title>
-                        </v-img>
+                <v-card-text class="text--primary text-center">
+                  <div>Upto 60% + Extra 10%</div>
+                  <div>Baggit, Zara, Fossil</div>
+                </v-card-text>
 
-                        <v-card-text class="text--primary text-center">
-                          <div>Upto 50%</div>
-                          <div>Zara, Selected, Celio</div>
-                        </v-card-text>
-
-                        <div class="text-center">
-                          <v-btn
-                            to="/search"
-                            class="ma-2"
-                            outlined
-                            color="info"
-                          >
-                            Explore
-                          </v-btn>
-                        </div>
-                      </v-card>
-                    </v-hover>
-                  </div>
-                  <div class="col-12 col-md-3 col-sm-6 col-xs-6 text-center">
-                    <v-hover v-slot:default="{ hover }" open-delay="200">
-                      <v-card :elevation="hover ? 16 : 2">
-                        <v-img
-                          class="white--text align-end"
-                          height="200px"
-                          :src="'https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg'"
-                        >
-                          <v-card-title>Jeans </v-card-title>
-                        </v-img>
-
-                        <v-card-text class="text--primary text-center">
-                          <div>Upto 60% + Extra 10%</div>
-                          <div>Jack & Jones, Levis</div>
-                        </v-card-text>
-
-                        <div class="text-center">
-                          <v-btn
-                            to="/search"
-                            class="ma-2"
-                            outlined
-                            color="info"
-                          >
-                            Explore
-                          </v-btn>
-                        </div>
-                      </v-card>
-                    </v-hover>
-                  </div>
-                  <div class="col-12 col-md-3 col-sm-6 col-xs-6 text-center">
-                    <v-hover v-slot:default="{ hover }" open-delay="200">
-                      <v-card :elevation="hover ? 16 : 2">
-                        <v-img
-                          class="white--text align-end"
-                          height="200px"
-                          :src="'https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg'"
-                        >
-                          <v-card-title>Shoes </v-card-title>
-                        </v-img>
-
-                        <v-card-text class="text--primary text-center">
-                          <div>Upto 60% + Extra 10%</div>
-                          <div>Nike, Adidas, Puma</div>
-                        </v-card-text>
-
-                        <div class="text-center">
-                          <v-btn
-                            to="/search"
-                            class="ma-2"
-                            outlined
-                            color="info"
-                          >
-                            Explore
-                          </v-btn>
-                        </div>
-                      </v-card>
-                    </v-hover>
-                  </div>
+                <div class="text-center">
+                  <v-btn to="/search" class="ma-2" outlined color="info">
+                    Explore
+                  </v-btn>
                 </div>
-              </v-card-text>
-            </v-col>
-          </v-row>
-        </v-container>
+              </v-card>
+            </v-hover>
+          </div>
+          <div class="col-12 col-md-3 col-sm-6 col-xs-6 text-center">
+            <v-hover v-slot:default="{ hover }" open-delay="200">
+              <v-card :elevation="hover ? 16 : 2">
+                <v-img
+                  class="white--text align-end"
+                  height="200px"
+                  :src="'https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg'"
+                >
+                  <v-card-title>T-Shirt </v-card-title>
+                </v-img>
+
+                <v-card-text class="text--primary text-center">
+                  <div>Upto 50%</div>
+                  <div>Zara, Selected, Celio</div>
+                </v-card-text>
+
+                <div class="text-center">
+                  <v-btn to="/search" class="ma-2" outlined color="info">
+                    Explore
+                  </v-btn>
+                </div>
+              </v-card>
+            </v-hover>
+          </div>
+          <div class="col-12 col-md-3 col-sm-6 col-xs-6 text-center">
+            <v-hover v-slot:default="{ hover }" open-delay="200">
+              <v-card :elevation="hover ? 16 : 2">
+                <v-img
+                  class="white--text align-end"
+                  height="200px"
+                  :src="'https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg'"
+                >
+                  <v-card-title>Jeans </v-card-title>
+                </v-img>
+
+                <v-card-text class="text--primary text-center">
+                  <div>Upto 60% + Extra 10%</div>
+                  <div>Jack & Jones, Levis</div>
+                </v-card-text>
+
+                <div class="text-center">
+                  <v-btn to="/search" class="ma-2" outlined color="info">
+                    Explore
+                  </v-btn>
+                </div>
+              </v-card>
+            </v-hover>
+          </div>
+          <div class="col-12 col-md-3 col-sm-6 col-xs-6 text-center">
+            <v-hover v-slot:default="{ hover }" open-delay="200">
+              <v-card :elevation="hover ? 16 : 2">
+                <v-img
+                  class="white--text align-end"
+                  height="200px"
+                  :src="'https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg'"
+                >
+                  <v-card-title>Shoes </v-card-title>
+                </v-img>
+
+                <v-card-text class="text--primary text-center">
+                  <div>Upto 60% + Extra 10%</div>
+                  <div>Nike, Adidas, Puma</div>
+                </v-card-text>
+
+                <div class="text-center">
+                  <v-btn to="/search" class="ma-2" outlined color="info">
+                    Explore
+                  </v-btn>
+                </div>
+              </v-card>
+            </v-hover>
+          </div>
+        </div>
+
         <v-card class="accent">
           <v-container>
             <v-row no-gutters>
