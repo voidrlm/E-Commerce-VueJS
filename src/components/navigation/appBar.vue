3e1596176694057ed94fc7ebf3cf710e6edce6e2
voidrlm <varjun609@gmail.com>
2023-01-29 03:48:46 +0530

    UI Improvement for cart

--- src/components/navigation/appBar.vue
+++ src/components/navigation/appBar.vue
@@ -1,72 +1,74 @@
 <template>
-  <v-app-bar
-    :color="$vuetify.theme.dark ? '#121212' : '#FFFFFF'"
-    app
-    class="pa-3"
-  >
-    <v-btn
-      v-if="$router.currentRoute.path !== '/dashboard'"
-      class="mt-n5 ml-n6"
-      :class="$vuetify.breakpoint.xsOnly ? 'mr-5' : ''"
-      icon
-      @click="$router.go(-1)"
-      ><v-icon>mdi-chevron-left-circle</v-icon></v-btn
+  <div>
+    <v-app-bar
+      :color="$vuetify.theme.dark ? '#121212' : '#FFFFFF'"
+      app
+      class="pa-3"
     >
-    <v-card-title
-      v-if="!$vuetify.breakpoint.xsOnly"
-      class="text-h5 mt-n5 ml-n4"
-      :style="'cursor:pointer'"
-      @click="routeToDashboard()"
-      >Store</v-card-title
-    >
-    <v-btn
-      class="mt-n5 ml-n6"
-      :class="$vuetify.breakpoint.xsOnly ? 'mr-5' : ''"
-      v-if="$vuetify.breakpoint.xsOnly"
-      icon
-      @click="routeToDashboard()"
-      ><v-icon size="40">mdi-home-circle</v-icon></v-btn
-    >
+      <v-btn
+        v-if="$router.currentRoute.path !== '/dashboard'"
+        class="mt-n5 ml-n6"
+        :class="$vuetify.breakpoint.xsOnly ? 'mr-5' : ''"
+        icon
+        @click="$router.go(-1)"
+        ><v-icon>mdi-chevron-left-circle</v-icon></v-btn
+      >
+      <v-card-title
+        v-if="!$vuetify.breakpoint.xsOnly"
+        class="text-h5 mt-n5 ml-n4"
+        :style="'cursor:pointer'"
+        @click="routeToDashboard()"
+        >Store</v-card-title
+      >
+      <v-btn
+        class="mt-n5 ml-n6"
+        :class="$vuetify.breakpoint.xsOnly ? 'mr-5' : ''"
+        v-if="$vuetify.breakpoint.xsOnly"
+        icon
+        @click="routeToDashboard()"
+        ><v-icon size="40">mdi-home-circle</v-icon></v-btn
+      >
 
-    <v-spacer />
-    <v-autocomplete
-      v-model="search"
-      :items="products"
-      placeholder="Search store"
-      filled
-      rounded
-      dense
-      :class="$vuetify.breakpoint.xsOnly ? '' : 'ml-5'"
-      class="accent--text mt-1"
-      prepend-inner-icon="mdi-magnify"
-      color="white"
-      hide-no-data
-      hide-selected
-      item-text="name"
-      item-value="id"
-      return-object
-      @input="searchProduct()"
-      ><template v-slot:item="{ item }">
-        <v-list-item-avatar
-          color="indigo"
-          class="text-h5 font-weight-light white--text"
-        >
-          <v-img :src="item.imgs[0]"></v-img>
-        </v-list-item-avatar>
-        <v-list-item-content>
-          <v-list-item-title v-text="item.name"></v-list-item-title>
-          <v-list-item-subtitle v-text="item.brand"></v-list-item-subtitle>
-        </v-list-item-content> </template
-    ></v-autocomplete>
-    <v-spacer /><v-btn
-      @click.stop="showCart = !showCart"
-      icon
-      class="mt-n5 mr-2"
-      ><v-icon>mdi-cart</v-icon></v-btn
-    >
-    <appBarMenu :class="$vuetify.breakpoint.xsOnly ? 'mr-n4' : ''" />
+      <v-spacer />
+      <v-autocomplete
+        v-model="search"
+        :items="products"
+        placeholder="Search store"
+        filled
+        rounded
+        dense
+        :class="$vuetify.breakpoint.xsOnly ? '' : 'ml-5'"
+        class="accent--text mt-1"
+        prepend-inner-icon="mdi-magnify"
+        color="white"
+        hide-no-data
+        hide-selected
+        item-text="name"
+        item-value="id"
+        return-object
+        @input="searchProduct()"
+        ><template v-slot:item="{ item }">
+          <v-list-item-avatar
+            color="indigo"
+            class="text-h5 font-weight-light white--text"
+          >
+            <v-img :src="item.imgs[0]"></v-img>
+          </v-list-item-avatar>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.name"></v-list-item-title>
+            <v-list-item-subtitle v-text="item.brand"></v-list-item-subtitle>
+          </v-list-item-content> </template
+      ></v-autocomplete>
+      <v-spacer /><v-btn
+        @click.stop="showCart = !showCart"
+        icon
+        class="mt-n5 mr-2"
+        ><v-icon>mdi-cart</v-icon></v-btn
+      >
+      <appBarMenu :class="$vuetify.breakpoint.xsOnly ? 'mr-n4' : ''" />
+    </v-app-bar>
     <cartBar :showShoppingCart="showCart" @hide="showCart = $event" />
-  </v-app-bar>
+  </div>
 </template>
 
 <script>
