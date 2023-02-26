b891f3ba1e039bbaa4f02e0a03dc368aa691e416
voidrlm <varjun609@gmail.com>
2023-02-26 16:53:37 +0530

    Store implementation for shopping cart items

--- src/store/index.js
+++ src/store/index.js
@@ -1,23 +1,32 @@
 import Vue from "vue";
 import Vuex from "vuex";
 import { encrypt } from "../services/encryptDecrypt";
-
+import VueCookies from "vue-cookies";
 Vue.use(Vuex);
-
+Vue.use(VueCookies);
 const store = new Vuex.Store({
   state: {
     currentUser: {},
+    shoppingCart: JSON.parse(localStorage.getItem("shoppingCartItems")) || [],
   },
   mutations: {
     setCurrentUser(state, currentUser) {
       return (state.currentUser = currentUser);
     },
+    setShoppingCartData(state, shoppingCart) {
+      return (state.shoppingCart = shoppingCart);
+    },
   },
+
   actions: {
     setCurrentUser(context, currentUser) {
       window.$cookies.set("uauth", encrypt(currentUser));
       if (context.currentUser === currentUser) return;
       context.commit("setCurrentUser", currentUser);
+    },
+    setShoppingCartData(context, shoppingCart) {
+      localStorage.setItem("shoppingCartItems", JSON.stringify(shoppingCart));
+      context.commit("setShoppingCartData", shoppingCart);
     },
 
     resetState() {
@@ -26,6 +35,7 @@ const store = new Vuex.Store({
   },
   getters: {
     currentUser: (state) => state.currentUser,
+    shoppingCart: (state) => state.shoppingCart,
   },
 });
 
