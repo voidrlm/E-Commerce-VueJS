4015db0b692a8e2f0db4cbbaaf91a0247e168228
voidrlm <varjun609@gmail.com>
2023-02-26 17:35:11 +0530

    Design changes for checkout page

--- src/views/cart.vue
+++ src/views/cart.vue
@@ -4,7 +4,7 @@
       <p class="text-h5 font-weight-light text-center pa-4">SHOPPING CART</p>
 
       <v-row>
-        <v-col :cols="12" md="9" sm="12">
+        <v-col :cols="12" md="8" sm="12">
           <v-card class="rounded-xl">
             <v-simple-table>
               <template v-slot:default>
@@ -71,39 +71,43 @@
             </v-simple-table></v-card
           >
         </v-col>
-        <v-col :cols="12" md="3" sm="12" style="background-color: lightgray">
-          <p class="headline">Order Summary</p>
-          <p class="overline">
-            Shipping and additional costs are calculated based on values you
-            have entered.
-          </p>
-          <v-simple-table>
-            <template v-slot:default>
-              <tbody>
-                <tr>
-                  <td>Order Subtotal</td>
-                  <td class="text-right" style="width: 50px">$160.00</td>
-                </tr>
-                <tr>
-                  <td>Shipping Charges</td>
-                  <td class="text-right" style="width: 50px">$10.00</td>
-                </tr>
-                <tr>
-                  <td>Tax</td>
-                  <td class="text-right" style="width: 50px">$5.00</td>
-                </tr>
-                <tr>
-                  <td>Total</td>
-                  <td class="text-right" style="width: 50px"><b>$175.00</b></td>
-                </tr>
-              </tbody>
-            </template>
-          </v-simple-table>
-          <div class="text-center">
-            <v-btn class="primary white--text mt-5" outlined
-              >PROCEED TO PAY</v-btn
-            >
-          </div>
+        <v-col :cols="12" md="4" sm="12">
+          <v-card class="rounded-xl pa-3">
+            <p class="text-h5 text-center">Order Summary</p>
+            <p class="caption text-center">
+              Shipping and additional costs are calculated based on values you
+              have entered.
+            </p>
+            <v-simple-table>
+              <template v-slot:default>
+                <tbody>
+                  <tr>
+                    <td>Order Subtotal</td>
+                    <td class="text-right" style="width: 50px">$160.00</td>
+                  </tr>
+                  <tr>
+                    <td>Shipping Charges</td>
+                    <td class="text-right" style="width: 50px">$10.00</td>
+                  </tr>
+                  <tr>
+                    <td>Tax</td>
+                    <td class="text-right" style="width: 50px">$5.00</td>
+                  </tr>
+                  <tr>
+                    <td>Total</td>
+                    <td class="text-right" style="width: 50px">
+                      <b>$175.00</b>
+                    </td>
+                  </tr>
+                </tbody>
+              </template>
+            </v-simple-table>
+            <div class="text-center">
+              <v-btn class="primary white--text mt-5" outlined
+                >PROCEED TO PAY</v-btn
+              >
+            </div></v-card
+          >
         </v-col>
       </v-row>
     </v-container>
